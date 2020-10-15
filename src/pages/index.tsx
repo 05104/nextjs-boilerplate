90df8a275c7d9e4e668a90dc16c19215c4c8f379
Richard Schott <richard@inoweb.com.br>
2020-10-15 10:03:30 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'Gay' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
